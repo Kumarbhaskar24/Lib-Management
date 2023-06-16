a31f72914657a1258c70e5d932e98cd41df4e7d9
Kumar <krbhaskar4321@gmail.com>
2023-06-16 11:13:02 +0530

    Basic UI of Home Componet is Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,59 +1,84 @@
-import React,{useState,useEffect} from "react";
-import {Link} from "react-router-dom";
-import {Box,Stack} from "@mui/system";
-import {Avatar, Typography,Card,Divider} from "@mui/material";
-import {AccountCircle} from "@mui/icons-material";
-const HomePage =()=>{
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Box, Stack } from "@mui/system";
+import EastIcon from "@mui/icons-material/East";
+import { Avatar, Typography, Card, Divider } from "@mui/material";
+import { AccountCircle, LibraryBooks } from "@mui/icons-material";
+const HomePage = () => {
     return (
         <Box p={2} ml={2}>
             {
-               <Stack direction="row" alignItems="center" justifyContent="space-between" >
-                <Typography>
-                    Home Page
-                </Typography>
+                <Stack direction="row" alignItems="center" justifyContent="space-between" >
+                    <Typography variant="h3" textAlign="center" sx={{mb:"20px"}}>
+                        Home Page
+                    </Typography>
+                </Stack>
 
-
-               </Stack>
-            
             }
             {
-               <Stack
-               direction="row"
-               spacing={2}
-               alignItems={"center"}
-               justifyContent={"center"}
-               >
-                <Card sx={{p:"20px",width:"250px"}}>
                 <Stack
-                    direction={"column"}
-                    alignItems={"center"}
-                    justifyContent={"space-around"}
-                    sx={{gap:"20px"}}
+                    direction="row"
+                    spacing={2}
+                    alignItems="center"
+                    justifyContent="center"
                 >
-
-                <Avatar sx={{bgcolor: "green",width:95,height:95}}>
-                    <AccountCircle fontSize="large"/>
-                </Avatar>
-                <Stack>
-                    <Typography varient="overline" style={{fontSize:"20px"}}>
-                    ADMIN LOGIN
-                    </Typography>
+                    <Card sx={{ p: "20px", width: "250px" }}>
+                        <Stack
+                            direction="column"
+                            alignItems="center"
+                            justifyContent="space-around"
+                            sx={{ gap: "20px" }}
+                        >
+                            <Avatar sx={{ bgcolor: "green", width: 95, height: 95 }}>
+                                <AccountCircle fontSize="large" />
+                            </Avatar>
+                            <Stack>
+                                <Typography variant="overline" style={{ fontSize: "20px" }}>
+                                    ADMIN LOGIN
+                                </Typography>
+                                <Typography variant="h5"></Typography>
+                            </Stack>
+                        </Stack>
+                        <Divider sx={{ mb: "10px", mt: "10px" }} />
+                        <Stack
+                            // component={Link}
+                            // to="/"
+                            direction="row"
+                            sx={{ textDecoration: "none" }}
+                        >
+                            <Typography>Login to Admin Dashboard</Typography>
+                            <EastIcon />
+                        </Stack>
+                    </Card>
+                    <Card sx={{ p: "20px", width: "250px" }}>
+                        <Stack
+                            direction="column"
+                            alignItems="center"
+                            justifyContent="space-around"
+                            sx={{ gap: "20px" }}
+                        >
+                            <Avatar sx={{ bgcolor: "orange", width: 100, height: 100 }}>
+                                <LibraryBooks fontSize="large" />
+                            </Avatar>
+                            <Stack>
+                                <Typography variant="overline" style={{ fontSize: "20px" }}>
+                                    LIBRARY BOOKS
+                                </Typography>
+                                <Typography variant="h5"></Typography>
+                            </Stack>
+                        </Stack>
+                        <Divider sx={{ mb: "10px", mt: "10px" }} />
+                        <Stack
+                            // component={Link}
+                            // to="/Library"
+                            direction="row"
+                            sx={{ textDecoration: "none" }}
+                        >
+                            <Typography>Go to Library</Typography>
+                            <EastIcon />
+                        </Stack>
+                    </Card>
                 </Stack>
-                </Stack>
-                <Divider sx={{mb:"10px",mt:"10px"}}/>
-                <Stack 
-                // component={Link}
-                // to="/"
-                direction="row"
-                sx={{textDecoration:"none"}}
-                >
-                    <Typography>
-                        Login To Admin DashBoard
-                    </Typography>
-                </Stack>
-                </Card>
-
-               </Stack> 
             }
         </Box>
     )
